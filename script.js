fdf9d7ac2f1ccdcb72ca5aa1ce1ce8bf2b7218a6
lizreznik <525ereznik@frhsd.com>
2024-03-22 10:39:22 -0400

    javascript creating list

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ function addName () {
     displayNames() // call the displayNames function to update the list
 
     nameInput.value = '' // clear the input field after adding the name 
-    
 }
+
+
+function displayNames() {
+    const nameList = document.getElementById('nameList')
+    nameList.innerHTML = '' //clear out the previous list items 
+
+    for (let i = 0; i < namesArray.length; i++) {
+        const name = namesArray[i]
+
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+
+        const span = document.createElement('span')
+        span.textContent = name
+
+        li.appendChild(span)
+        nameList.appendChild(li)
+    }
+}
+
+document.getElementById('addNameBtn').addEventListener('click', addName)
